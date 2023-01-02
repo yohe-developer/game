6111cee14ac065abfe17d9b3a5aa386b99e8a8d5
aio <aioet@qq.com>
2023-01-02 14:06:26 +0800

    feat: 字体对齐

--- src/application/src/math2d.ts
+++ src/application/src/math2d.ts
@@ -39,3 +39,43 @@ export class Vec2 {
     return new Vec2(x, y)
   }
 }
+
+export class Size {
+  public values: Float32Array
+  public constructor(w: number = 1, h: number = 1) {
+    this.values = new Float32Array([w, h])
+  }
+
+  public set width(value: number) {
+    this.values[0] = value
+  }
+
+  public get width(): number {
+    return this.values[0]
+  }
+
+  public set height(value: number) {
+    this.values[1] = value
+  }
+
+  public get height(): number {
+    return this.values[1]
+  }
+
+  public static create(w: number = 1, h: number = 1): Size {
+    return new Size(w, h)
+  }
+}
+
+export class Rectangle {
+  public origin: Vec2
+  public size: Size
+  public constructor(origin: Vec2 = new Vec2(), size: Size = new Size(1, 1)) {
+    this.origin = origin
+    this.size = size
+  }
+
+  public static create(x: number = 0, y: number = 0, w: number = 1, h: number = 1): Rectangle {
+    return new Rectangle(new Vec2(x, y), new Size(w, h))
+  }
+}
--- src/application/test/TestApplication.ts
+++ src/application/test/TestApplication.ts
@@ -3,6 +3,64 @@
  */
 import TEST_IMG from '../data/test.jpg'
 import { Canvas2DApplication } from '@/application/src/Canvas2DApplication'
+import { Rectangle, Size, Vec2 } from '../src/math2d'
+
+type TextAlign = 'start' | 'left' | 'center' | 'right' | 'end'
+
+type TextBaseline = 'alphabetic' | 'hanging' | 'top' | 'middle' | 'bottom'
+
+type FontType = '10px sans-serif' | '15px sans-serif' | '20px sans-serif' | '25px sans-serif'
+
+type FontStyle = 'normal' | 'italic' | 'oblique'
+
+type FontVariant = 'normal' | 'small-caps'
+
+type FontWeight =
+  | 'normal'
+  | 'bold'
+  | 'bolder'
+  | 'lighter'
+  | '100'
+  | '200'
+  | '300'
+  | '400'
+  | '500'
+  | '600'
+  | '700'
+  | '800'
+  | '900'
+
+type FontSize =
+  | '10px'
+  | '12px'
+  | '16px'
+  | '18px'
+  | '24px'
+  | '50%'
+  | '75%'
+  | '100%'
+  | '125%'
+  | '150%'
+  | 'xx-small'
+  | 'x-small'
+  | 'small'
+  | 'medium'
+  | 'large'
+  | 'x-large'
+  | 'xx-large'
+
+type FontFamily = 'sans-serif' | 'serif' | 'courier' | 'fantasy' | 'monospace'
+export enum ELayout {
+  LEFT_TOP,
+  RIGHT_TOP,
+  RIGHT_BOTTOM,
+  LEFT_BOTTOM,
+  CENTER_MIDDLE,
+  CENTER_TOP,
+  RIGHT_MIDDLE,
+  CENTER_BOTTOM,
+  LEFT_MIDDLE
+}
 
 type PatternRepeat = 'repeat' | 'repeat-x' | 'repeat-y' | 'no-repeat'
 export class TestApplication extends Canvas2DApplication {
@@ -29,7 +87,9 @@ export class TestApplication extends Canvas2DApplication {
     // this.fillPatternRect(0, 0, this.canvas.width, this.canvas.height)
     // this.fillPatternRect(0, 0, this.canvas.width, this.canvas.height, 'no-repeat')
     // this.strokeCoord(12, 12, this.canvas.width, this.canvas.height)
-    this.strokeGrid()
+    // this.strokeGrid()
+    // this.testCanvas2DTextLayout()
+    this.testMyTextLayout()
   }
 
   public timeCallback(id: number, data: any): void {
@@ -208,6 +268,21 @@ export class TestApplication extends Canvas2DApplication {
     }
   }
 
+  public strokeRect(x: number, y: number, w: number, h: number, color: string = 'black'): void {
+    if (this.context2D !== null) {
+      this.context2D.save()
+      this.context2D.strokeStyle = color
+      this.context2D.beginPath()
+      this.context2D.moveTo(x, y)
+      this.context2D.lineTo(x + w, y)
+      this.context2D.lineTo(x + w, y + h)
+      this.context2D.lineTo(x, y + h)
+      this.context2D.closePath()
+      this.context2D.stroke()
+      this.context2D.restore()
+    }
+  }
+
   public fillCircle(
     x: number,
     y: number,
@@ -222,5 +297,311 @@ export class TestApplication extends Canvas2DApplication {
       this.context2D.fill()
       this.context2D.restore()
     }
+  }
+
+  public fillText(
+    text: string,
+    x: number,
+    y: number,
+    color: string = 'white',
+    align: CanvasTextAlign = 'left',
+    baseline: TextBaseline = 'top',
+    font: string = '10px sans-serif'
+  ): void {
+    if (this.context2D !== null) {
+      this.context2D.save()
+      this.context2D.textAlign = align
+      this.context2D.textBaseline = baseline
+      this.context2D.font = font
+      this.context2D.fillStyle = color
+      this.context2D.fillText(text, x, y)
+      this.context2D.restore()
+    }
+  }
+
+  public calcTextSize(text: string, char: string = 'W', scale: number = 0.5): Size {
+    if (this.context2D !== null) {
+      const size: Size = new Size()
+      // 先设置 this.context2D.font 可以获取匹配的大小
+      size.width = this.context2D.measureText(text).width
+      const w: number = this.context2D.measureText(char).width
+      size.height = w + w * scale
+      return size
+    }
+    throw new Error('context2D is null')
+  }
+
+  public calcLocalTextRectangle(
+    layout: ELayout,
+    text: string,
+    parentWidth: number,
+    parentHeight: number
+  ): Rectangle {
+    const s: Size = this.calcTextSize(text)
+    const o: Vec2 = Vec2.create()
+    const left: number = 0
+    const top: number = 0
+    const right: number = parentWidth - s.width
+    const bottom: number = parentHeight - s.height
+    const center: number = right * 0.5
+    const middle: number = bottom * 0.5
+    switch (layout) {
+      case ELayout.LEFT_TOP:
+        o.x = left
+        o.y = top
+        break
+      case ELayout.RIGHT_TOP:
+        o.x = right
+        o.y = top
+        break
+      case ELayout.RIGHT_BOTTOM:
+        o.x = right
+        o.y = bottom
+        break
+      case ELayout.LEFT_BOTTOM:
+        o.x = left
+        o.y = bottom
+        break
+      case ELayout.CENTER_MIDDLE:
+        o.x = center
+        o.y = middle
+        break
+      case ELayout.CENTER_TOP:
+        o.x = center
+        o.y = 0
+        break
+      case ELayout.RIGHT_MIDDLE:
+        o.x = right
+        o.y = middle
+        break
+      case ELayout.CENTER_BOTTOM:
+        o.x = center
+        o.y = bottom
+        break
+      case ELayout.LEFT_MIDDLE:
+        o.x = left
+        o.y = middle
+        break
+    }
+    return new Rectangle(o, s)
+  }
+
+  public fillRectWithTitle(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    title: string = '',
+    layout: ELayout = ELayout.CENTER_MIDDLE,
+    color: string = 'grey',
+    showCoord: boolean = true
+  ): void {
+    if (this.context2D !== null) {
+      this.context2D.save()
+      this.context2D.fillStyle = color
+      this.context2D.beginPath()
+      this.context2D.rect(x, y, width, height)
+      this.context2D.fill()
+      if (title.length !== 0) {
+        const rect: Rectangle = this.calcLocalTextRectangle(layout, title, width, height)
+        this.fillText(
+          title,
+          x + rect.origin.x,
+          y + rect.origin.y,
+          'white',
+          'left',
+          'top' /*, '10px sans-serif' */,
+          this.makeFontString('18px', 'bold', 'italic', 'small-caps', 'sans-serif')
+        )
+        this.strokeRect(
+          x + rect.origin.x,
+          y + rect.origin.y,
+          rect.size.width,
+          rect.size.height,
+          'pink'
+        )
+        this.fillCircle(x + rect.origin.x, y + rect.origin.y, 2)
+      }
+      if (showCoord) {
+        this.strokeCoord(x, y, width + 20, height + 20)
+        this.fillCircle(x, y, 3)
+      }
+
+      this.context2D.restore()
+    }
+  }
+
+  public testCanvas2DTextLayout(): void {
+    const x: number = 20
+    const y: number = 20
+    const width: number = this.canvas.width - x * 2
+    const height: number = this.canvas.height - y * 2
+    let drawX: number = x
+    let drawY: number = y
+    const radius: number = 3
+
+    this.fillRectWithTitle(x, y, width, height)
+    this.fillText('left-top', drawX, drawY, 'pink', 'left', 'top' /*, '20px sans-serif' */)
+    this.fillCircle(drawX, drawY, radius, 'black')
+
+    drawX = x + width
+    drawY = y
+    this.fillText('right-top', drawX, drawY, 'white', 'right', 'top' /* , '20px sans-serif' */)
+    this.fillCircle(drawX, drawY, radius, 'black')
+
+    drawX = x + width
+    drawY = y + height
+    this.fillText('right-bottom', drawX, drawY, 'white', 'right', 'bottom')
+    this.fillCircle(drawX, drawY, radius, 'black')
+
+    drawX = x + width * 0.5
+    drawY = y + height * 0.5
+    this.fillText(
+      'center-middle',
+      drawX,
+      drawY,
+      'black',
+      'center',
+      'middle',
+      this.makeFontString('18px', 'bold', 'italic', 'small-caps', 'sans-serif')
+    )
+    this.fillCircle(drawX, drawY, radius, 'black')
+  }
+
+  public testMyTextLayout(
+    font: string = this.makeFontString('10px', 'normal', 'normal', 'normal', 'sans-serif')
+  ): void {
+    const x: number = 20
+    const y: number = 20
+    const width: number = this.canvas.width - x * 2
+    const height: number = this.canvas.height - y * 2
+    const right: number = x + width
+    const bottom: number = y + height
+
+    let drawX: number = x
+    let drawY: number = y
+    const drawWidth: number = 150
+    const drawHeight: number = 150
+
+    if (this.context2D !== null) {
+      this.context2D.save()
+      this.context2D.font = font
+      this.fillRectWithTitle(x, y, width, height)
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'left-top',
+        ELayout.LEFT_TOP,
+        'rgba( 255 , 255 , 0 , 0.2 )'
+      )
+      drawX = right - drawWidth
+      drawY = y
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'right-top',
+        ELayout.RIGHT_TOP,
+        'rgba( 255 , 255 , 0 , 0.2 )'
+      )
+      drawX = right - drawWidth
+      drawY = bottom - drawHeight
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'right-bottom',
+        ELayout.RIGHT_BOTTOM,
+        'rgba( 255 , 255 , 0 , 0.2 )'
+      )
+      drawX = x
+      drawY = bottom - drawHeight
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'left-bottom',
+        ELayout.LEFT_BOTTOM,
+        'rgba( 255 , 255 , 0 , 0.2 )'
+      )
+      drawX = (right - drawWidth) * 0.5
+      drawY = (bottom - drawHeight) * 0.5
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'center-middle',
+        ELayout.CENTER_MIDDLE,
+        'rgba( 255 , 0 , 0 , 0.2 )'
+      )
+      drawX = (right - drawWidth) * 0.5
+      drawY = y
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'center-top',
+        ELayout.CENTER_TOP,
+        'rgba( 0 , 255 , 0 , 0.2 )'
+      )
+      drawX = right - drawWidth
+      drawY = (bottom - drawHeight) * 0.5
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'right-middle',
+        ELayout.RIGHT_MIDDLE,
+        'rgba( 0 , 255 , 0 , 0.2 )'
+      )
+      drawX = (right - drawWidth) * 0.5
+      drawY = bottom - drawHeight
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'center-bottom',
+        ELayout.CENTER_BOTTOM,
+        'rgba( 0 , 255 , 0 , 0.2 )'
+      )
+      drawX = x
+      drawY = (bottom - drawHeight) * 0.5
+      this.fillRectWithTitle(
+        drawX,
+        drawY,
+        drawWidth,
+        drawHeight,
+        'left-middle',
+        ELayout.LEFT_MIDDLE,
+        'rgba( 0 , 255 , 0 , 0.2 )'
+      )
+    }
+  }
+
+  public /* static */ makeFontString(
+    size: FontSize = '10px',
+    weight: FontWeight = 'normal',
+    style: FontStyle = 'normal',
+    variant: FontVariant = 'normal',
+    family: FontFamily = 'sans-serif'
+  ): string {
+    const strs: string[] = []
+    strs.push(style)
+    strs.push(variant)
+    strs.push(weight)
+    strs.push(size)
+    strs.push(family)
+    const ret: string = strs.join(' ')
+    console.log(ret)
+    return ret
   }
 }
